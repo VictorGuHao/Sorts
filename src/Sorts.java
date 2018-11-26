e760e4b1f14277637e71186c0812962c85c1375b
VictorGuHao <foruze2001@gmail.com>
2018-11-26 14:36:24 -0500

    Add string array list sort method

--- src/Sorts.java
+++ src/Sorts.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Sorts
 {
     public static void ascendingSort(int[] arr)
@@ -60,6 +62,20 @@ public class Sorts
                     lowNumPos = j;
             }
             swap(i, lowNumPos, arr);
+        }
+    }
+
+    public static void ascendingSort(ArrayList<String> arr)
+    {
+        for(int i = 0; i < arr.size() - 1; i++)
+        {
+            int lowNumPos = i;
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                if (arr.get(j).compareTo(arr.get(lowNumPos)) < 0)
+                    lowNumPos = j;
+            }
+            arr.set(i, arr.set(lowNumPos, arr.get(i)));
         }
     }
 
